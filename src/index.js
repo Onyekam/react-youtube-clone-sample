25bfaed1e60908e46a3177e5ccdc293151a7440a
Michael <onyekami25@gmail.com>
2022-04-03 16:04:22 +0100

    playing selected videos

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
-
+import VideoList from "./components/video_list";
 import SearchBar from "./components/search_bar";
+import VideoDetail from "./components/video_detail";
 
 const API_KEY = "AIzaSyD_y44r9QBm6quV0xW-E0UmTEjul2fxt34";
-YTSearch({ key: API_KEY, term: "surfboards" }, function (data) {
-  console.log(data);
-});
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
+    YTSearch({ key: API_KEY, term: "surfboards" }, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0],
+      });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
+}
 // Take this component's html and put iot on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector(".container"));
