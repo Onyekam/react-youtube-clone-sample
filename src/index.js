9816916388827913ddbe5406aa161e90afb21666
Michael <onyekami25@gmail.com>
2022-04-03 14:48:33 +0100

    added youtube search api functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// Create a new component. This component should produce some html
+import YTSearch from "youtube-api-search";
+
+import SearchBar from "./components/search_bar";
+
+const API_KEY = "AIzaSyD_y44r9QBm6quV0xW-E0UmTEjul2fxt34";
+YTSearch({ key: API_KEY, term: "surfboards" }, function (data) {
+  console.log(data);
+});
+
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 // Take this component's html and put iot on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector(".container"));
