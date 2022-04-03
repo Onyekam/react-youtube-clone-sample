86b5bac946c79ca0275eea806ad4d18a14458198
Michael <onyekami25@gmail.com>
2022-04-03 13:57:21 +0100

    Project Structure Setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+import React from "react";
+import ReactDOM from "react-dom";
+// Create a new component. This component should produce some html
+const App = () => {
+  return <div>Hi!</div>;
+};
+// Take this component's html and put iot on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector(".container"));
